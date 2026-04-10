e48cf4fdf1f0da3194c04fa6eb104c5222b81bab
edugrowth5-rgb <edugrowth5@gmail.com>
2026-04-10 21:22:47 +0530

    Create Msw.js

---
+++ Msw.js
@@ -0,0 +1,45 @@
+// Msw.js - Service Worker for Maths Magic PWA
+
+const CACHE_NAME = 'maths-magic-v1';
+// Un sabhi files ki list jo offline chalne ke liye chahiye
+const ASSETS_TO_CACHE = [
+    'Mindex.html',
+    'Mstyle.css',
+    'Mapp.js',
+    'Mmanifest.json',
+    'icon-192.png',
+    'icon-512.png',
+    'https://fonts.googleapis.com/css2?family=Fredoka:wght@400;600&display=swap'
+];
+
+// 1. Install Event: Files ko cache mein save karna
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('M-Project: Caching shell assets');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// 2. Activate Event: Purane cache ko delete karna (jab aap update karein)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// 3. Fetch Event: Internet na hone par cache se files dena
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Agar file cache mein hai toh wahi de do, nahi toh network se lo
+            return response || fetch(event.request);
+        })
+    );
+});
